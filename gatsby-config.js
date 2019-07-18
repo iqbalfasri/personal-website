ac296f879d12d280296ce57a76c631ccfd696db4
Iqbal Fasri <iqbalx64@gmail.com>
2019-07-18 15:21:11 +0700

    Config for creating blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,17 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Iqbal Fasri`,
+    description: `My Personal website.`,
     author: `@gatsbyjs`,
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
     `gatsby-plugin-netlify`,
     `gatsby-plugin-react-helmet`,
     {
