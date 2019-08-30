56c3728277672fe695c0895d347b6810fef03810
Iqbal Fasri <iqbalx64@gmail.com>
2019-08-30 11:12:27 +0700

    Add styled component for gatsbyjs

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-netlify`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
