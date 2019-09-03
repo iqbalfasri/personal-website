7a95ad2338aa1aea203a79e7476185d393ed7623
Iqbal Fasri <iqbalx64@gmail.com>
2019-09-03 14:57:51 +0700

    add: add google analytics tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-115189072-1`,
       },
     },
     {
