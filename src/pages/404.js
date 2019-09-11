360b7261944859bcb04e5d4b97945bfef1df3f7e
Iqbal Fasri <iqbalx64@gmail.com>
2019-09-11 15:06:36 +0700

    add: styling not found page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,16 @@
 import React from "react"
 
-import Layout from "../components/LayoutComponent"
 import SEO from "../components/SeoComponent"
+import Layout from "../components/LayoutComponent"
+import {  } from "../components/ButtonComponent"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404 Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="notfound-wrapper">
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </div>
   </Layout>
 )
 
