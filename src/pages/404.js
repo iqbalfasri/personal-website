bf247fda281fe679a3e82d974d72472e6912ae3d
Iqbal Fasri <iqbalx64@gmail.com>
2019-09-12 15:18:41 +0700

    fix: fix code 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,16 +2,17 @@ import React from "react"
 
 import SEO from "../components/SeoComponent"
 import Layout from "../components/LayoutComponent"
-import {  } from "../components/ButtonComponent"
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404 Not found" />
-    <div className="notfound-wrapper">
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
-  </Layout>
-)
+const NotFoundPage = () => {
+  return (
+    <Layout>
+      <SEO title="404 Not found" />
+      <div className="notfound-wrapper">
+        <h1>NOT FOUND</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </div>
+    </Layout>
+  )
+}
 
 export default NotFoundPage
