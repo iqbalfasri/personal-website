7abf9b96fdd904ad2d9fecba03364c5b89b001f9
Iqbal Fasri <iqbalx64@gmail.com>
2020-04-05 01:06:40 +0700

    Home screen re-design

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,44 @@
-import React from "react"
+import React, { useEffect } from "react"
+import Splitting from "splitting"
 
 import Layout from "../components/LayoutComponent"
 import SEO from "../components/SeoComponent"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <div className="main-page">
-      <img
-        alt="Ilustrasi"
-        loading="lazy"
-        className="ilustration"
-        src={require("../assets/images/under_construct@2x.svg")}
-      />
-      <h1>Nanti dulu ya, websitenya lagi dibuat</h1>
-      <div className="contact">
-        <p>Kalau ada keperluan, bisa kontak saya</p>
-        <a href="mailto:iqbalx64@gmail.com">iqbalx64@gmail.com</a>
-      </div>
-    </div>
-  </Layout>
+import "../assets/css/pages/index.css"
+
+const CircleText = ({text}) => (
+  <div class="circle-me" data-splitting>{text}</div>
 )
+
+const IndexPage = () => {
+  useEffect(() => {
+    Splitting({
+      by: "chars",
+      whitespace: true
+    });
+
+    return () => {
+      Splitting({
+        by: "chars",
+        whitespace: true
+      });
+    }
+  }, [])
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div className="main-page">
+        <div className="container">
+        <CircleText text="Welcome to my website" />
+          <div className="text--container">
+            <h1>My name is <br />Muhammad Iqbal Fasri</h1>
+            <h1 className="my-5">i'm a <br />Frontend Developer</h1>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
